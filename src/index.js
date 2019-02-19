0149c404625367ea487bb7b4f55fa1e73d68ec1d
PansevichVitali <egyptiain94@gmail.com>
2019-02-19 21:33:40 +0300

    implementation task 2

--- src/index.js
+++ src/index.js
@@ -4,4 +4,12 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let count = 0;
+  for (let i = 0; i<preferences.length; i++){
+    let trianglePassage=preferences[i];
+    trianglePassage = preferences[trianglePassage-1];
+    if(preferences[trianglePassage-1]-1 === i && i !== preferences[i]-1)
+      count++;
+  }
+  return count/3;
 };
