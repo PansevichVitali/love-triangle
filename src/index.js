664bc22ee417e15657b5d4e5f6e5ea5a9b4bf745
PansevichVitali <egyptiain94@gmail.com>
2019-02-20 14:45:43 +0300

    refactoring code, delete comments

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-/**
- * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
- */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
   let count = 0;
-  for (let i = 0; i<preferences.length; i++){
-    let trianglePassage=preferences[i];
+  for (let i = 0, trianglePassage; i < preferences.length; i++){
+    trianglePassage = preferences[i];
     trianglePassage = preferences[trianglePassage-1];
     if(preferences[trianglePassage-1]-1 === i && i !== preferences[i]-1)
       count++;
